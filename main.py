949d17e3998bac58a312a65f63d789aa23384c04
Ahmed-Elarosi <cabry95@protonmail.com>
2024-06-10 23:09:12 +0200

    email config

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import schedule
 import time
+
+# Email server configuration
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 587  # For TL .
+EMAIL_ADDRESS = 'ah.hamdi.mohi@gmail.com'
+EMAIL_PASSWORD = '*********'
+
