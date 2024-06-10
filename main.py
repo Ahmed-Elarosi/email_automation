d8d8080d75e8d690e29137be48d1620af676b977
Ahmed-Elarosi <cabry95@protonmail.com>
2024-06-10 23:10:19 +0200

    email conten

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ SMTP_PORT = 587  # For TL .
 EMAIL_ADDRESS = 'ah.hamdi.mohi@gmail.com'
 EMAIL_PASSWORD = '*********'
 
+# Function to create email content
+def create_email(subject, body):
+    msg = MIMEMultipart()
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = EMAIL_ADDRESS
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+    return msg
+
